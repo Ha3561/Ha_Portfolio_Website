9e40ce9bc5889d89984d0f41d3ff23ab54477b0d
Ha3561 <onmailharshit@gmail.com>
2024-06-24 21:44:05 +0530

    Modified Contact Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import About from "./components/About"
 import Technologies from "./components/Technologies" 
 import Experience from "./components/Experience" 
 import Projects from "./components/Projects" 
-import Contact from  "./components/Contact"
+import Contact from  "./components/Contact_Me"
 
 export default function Home() {
   return ( 
